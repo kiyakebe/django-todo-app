df9c9f15ede731bbd1ed84533f737804cc3a8065
kiya kebe <kiyakebe799@gmail.com>
2023-11-23 15:00:42 +0200

    created toto list template

--- main/models.py
+++ main/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class ToDoList(models.Model):
     name = models.CharField(max_length=200)
     
+    objects = models.Manager()
+    
     def __str__(self):
         return str(self.name)
 
@@ -11,6 +13,8 @@ class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     complete = models.BooleanField()
+    
+    objects = models.Manager()
     
     def __str__(self):
         return str(self.text)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import ToDoList, Item
 
-# Create your views here.
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls": ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
