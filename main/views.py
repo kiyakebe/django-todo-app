683443574fcd8d43670e65a64d03345d868b11ca
kiya kebe <kiyakebe799@gmail.com>
2023-11-24 20:02:04 +0200

    added: sidebar

--- main/views.py
+++ main/views.py
@@ -1,28 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import ToDoList, Item
+from .models import ToDoList
 from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    
     if response.method == "POST":
         print(response.POST)
         if response.POST.get("save"):
             for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id) == "clicked"):
+                if response.POST.get("c" + str(item.id)) == "clicked":
                     item.complete = True
                 else:
                     item.complete = False
-                item.save()
+                item.save()     
         elif response.POST.get("newItem"):
             txt = response.POST.get("new")
             if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=True)
+                ls.item_set.create(text=txt, complete=False)
             else:
                 print("Invalid Input")
     
     return render(response, "main/list.html", {"ls": ls})
+
 
 def home(response):
     return render(response, "main/home.html", {})
